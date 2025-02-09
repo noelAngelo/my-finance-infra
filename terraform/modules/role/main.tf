f5fa8ed91786049556dfbfe72ad63dcad355adf5
Noel Borneo <born.angelonoel@gmail.com>
2025-02-09 14:02:34 +1100

    [MFI-5] Added role

--- terraform/modules/role/main.tf
+++ terraform/modules/role/main.tf
@@ -18,4 +18,5 @@ resource "snowflake_grant_privileges_to_database_role" "db_role_privileges" {
   database_role_name = snowflake_database_role.dlt_loader_role.fully_qualified_name
   on_database        = snowflake_database_role.dlt_loader_role.database
   all_privileges = true
+  
 }
