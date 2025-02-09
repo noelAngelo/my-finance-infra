6f2cda439eb682bcccd1c64db1f5b13390741ec1
Noel Borneo <born.angelonoel@gmail.com>
2025-02-09 13:19:40 +1100

    [MFI-4] Added DB and schema

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -1,12 +1,12 @@
-terraform { 
-  cloud { 
-    
-    organization = "noelzy-org" 
+terraform {
+  cloud {
 
-    workspaces { 
-      name = "my-finance-infra" 
-    } 
-  } 
+    organization = "noelzy-org"
+
+    workspaces {
+      name = "my-finance-infra"
+    }
+  }
 }
 
 #------------------------------------------------------------------------------
@@ -14,15 +14,15 @@ terraform {
 #------------------------------------------------------------------------------
 
 module "snowflake_warehouse" {
-  source             = "../../modules/warehouse"
-  project            = var.project
-  environment        = var.environment
-  
-  snowflake_region   = var.snowflake_region
-  snowflake_account  = var.snowflake_account
-  snowflake_role     = var.snowflake_role
+  source      = "../../modules/warehouse"
+  project     = var.project
+  environment = var.environment
 
-  snowflake_username = var.snowflake_username
-  snowflake_password = var.snowflake_password
+  snowflake_region  = var.snowflake_region
+  snowflake_account = var.snowflake_account
+  snowflake_role    = var.snowflake_role
+
+  snowflake_username          = var.snowflake_username
+  snowflake_password          = var.snowflake_password
   snowflake_organization_name = var.snowflake_organization_name
 }
--- terraform/modules/warehouse/main.tf
+++ terraform/modules/warehouse/main.tf
@@ -1,6 +1,20 @@
 resource "snowflake_warehouse" "this" {
-  name              = replace("${var.project}-${upper(var.environment)}-${var.warehouse_name}", "-", "_")
+  name              = replace(upper("${var.project}-${var.environment}-WH"), "-", "_")
   warehouse_size    = upper(var.warehouse_size)
   auto_suspend      = var.auto_suspend
   auto_resume       = var.auto_resume
+}
+
+# Create finance analytics database
+resource "snowflake_database" "analytics" {
+  name                        = replace(upper("${var.project}-${var.environment}-ANALYTICS"), "-", "_")
+  comment                     = "Personal finance analytics database"
+  data_retention_time_in_days = 90
+}
+
+# Create RAW schema for DLT loading
+resource "snowflake_schema" "raw" {
+  database   = snowflake_database.analytics.name
+  name       = "RAW"
+  comment    = "Raw data files loaded via DLT"
 }
