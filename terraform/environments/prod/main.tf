203976664d4cf5f79e712a95eb2468328526f8af
Noel Borneo <born.angelonoel@gmail.com>
2025-02-09 13:40:26 +1100

    [MFI-4] Added role

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -26,3 +26,21 @@ module "snowflake_warehouse" {
   snowflake_password          = var.snowflake_password
   snowflake_organization_name = var.snowflake_organization_name
 }
+
+#------------------------------------------------------------------------------
+# Create Snowflake Role for DLT Loader
+#------------------------------------------------------------------------------
+
+module "snowflake_role" {
+  source      = "../../modules/role"
+
+  snowflake_region  = var.snowflake_region
+  snowflake_account = var.snowflake_account
+  snowflake_role    = var.snowflake_role
+
+  snowflake_username          = var.snowflake_username
+  snowflake_password          = var.snowflake_password
+  snowflake_organization_name = var.snowflake_organization_name
+
+  database = module.snowflake_warehouse.finance_database_name
+}
