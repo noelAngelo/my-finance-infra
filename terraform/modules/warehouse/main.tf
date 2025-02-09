2ed59b45e55728be4f450104f344a1cd6a451c57
Noel Borneo <born.angelonoel@gmail.com>
2025-02-09 12:18:42 +1100

    [MFI-4] Added workspace

--- terraform/modules/warehouse/main.tf
+++ terraform/modules/warehouse/main.tf
@@ -1,5 +1,5 @@
 resource "snowflake_warehouse" "this" {
-  name              = "${var.project}-${upper(var.environment)}-${var.warehouse_name}"
+  name              = replace("${var.project}-${upper(var.environment)}-${var.warehouse_name}", "-", "_")
   warehouse_size    = upper(var.warehouse_size)
   auto_suspend      = var.auto_suspend
   auto_resume       = var.auto_resume
